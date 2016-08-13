ebc93a820fe6952b841a61b9c6f0933cca7ee721
william <william.espinosa@lambda3.com.br>
2016-08-12 21:53:11 -0300

    Criação de solution, projeto e projeto de testes com xnuit

---
+++ FaixasSalariais.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,23 @@
+﻿using FaixasSalariais.Controllers;
+using System.Web.Mvc;
+using Xunit;
+
+namespace FaixasSalariais.Tests.Controllers
+{
+    public class HomeControllerTest
+    {
+        [Fact]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+  
+            // Assert
+            Assert.NotNull(result);
+        }
+    }
+}
