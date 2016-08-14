bf92e72d1c708884ba3442b13f7fe2246bd0ae84
william <william.espinosa@lambda3.com.br>
2016-08-14 19:08:00 -0300

    Chain of responsibilty

--- FaixasSalariais/Models/FaixaSalarial.cs
+++ FaixasSalariais/Models/FaixaSalarial.cs
@@ -1,21 +1,49 @@
 ﻿namespace FaixasSalariais.Models
 {
-    public class FaixaSalarial
+    public abstract class FaixaSalarial
     {
         public readonly decimal Minimo;
-        public readonly decimal Maximo;
+        public readonly decimal? Maximo;
         public readonly CalculoImposto Calculo;
-
-        public FaixaSalarial(decimal minimo, decimal maximo, CalculoImposto calculo)
+        public readonly FaixaSalarial Proximo;
+        public FaixaSalarial(FaixaSalarial proximo, decimal minimo, decimal? maximo, CalculoImposto calculo)
         {
             Minimo = minimo;
             Maximo = maximo;
             Calculo = calculo;
+            Proximo = proximo;
         }
 
-        public bool EstaContidoNaFaixa(decimal salario)
+        public FaixaSalarial DefinirFaixa(decimal salario)
         {
-            return salario >= Minimo && salario < Maximo;
+            if (salario >= Minimo && salario < Maximo)
+                return this;
+
+            return Proximo.DefinirFaixa(salario);
+        }
+    }
+
+    public class FaixaSalarialUm : FaixaSalarial
+    {
+        public FaixaSalarialUm()
+            : base(new FaixaSalarialDois(), 0, 600, new CalculoImposto(0, 0))
+        {
+        }
+    }
+
+    public class FaixaSalarialDois : FaixaSalarial
+    {
+        public FaixaSalarialDois()
+            : base(new FaixaSalarialTres(), 600, 2000, new CalculoImposto(300, 0.05M))
+        {
+        }
+    }
+
+    public class FaixaSalarialTres : FaixaSalarial
+    {
+        public FaixaSalarialTres()
+            : base(null, 2000, 3000, new CalculoImposto(300, 0.1M))
+        {
         }
     }
 }
